a29e23d121907bda6e2171aa03721764bf68b3e7
Cameron Chandler <webstormcam@gmail.com>
2022-03-30 11:51:28 -0400

    Lets see what the boss thinks

--- script.js
+++ script.js
@@ -37,7 +37,7 @@ function showSlides(n) {
 }
 
 
-var map = L.map('map').setView([51.505, -0.09], 13);
+var map = L.map('map').setView([37.8495892, -84.3684298], 10);
 
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
